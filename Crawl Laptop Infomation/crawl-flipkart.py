27385fba36c550820a97acb7c3cb19b9cc5a89f3
nhan-skytech <Nhan.TCH@skytechkey.com>
2021-05-11 23:30:48 +0700

    update

--- Crawl Laptop Infomation/crawl-flipkart.py
+++ Crawl Laptop Infomation/crawl-flipkart.py
@@ -9,25 +9,151 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import openpyxl
 
+convert_generals = []
+chip_brand = []
 chip_brands = []
+chip_speed = []
 chip_speeds = []
+chip_type = []
 chip_types = []
+hdd = []
 hdds = []
+ssd = []
 ssds = []
+card_brand = []
 card_brands = []
+card_model = []
 card_models = []
-card_onboards = []
-card_onboard_models = []
+pricee = []
 prices = []
+ram = []
 rams = []
+ram_type = []
 ram_types = []
+ram_speed = []
 ram_speeds = []
+inch = []
 inches = []
 descs = []
 
+def save(num_cv):
+    a = {'Description': descs, 'Chip Brands': chip_brands, 'Chip Speeds': chip_speeds, 'Chip Type': chip_types,
+         'Ram': rams, 'Ram Type': ram_types, 'Ram Speed': ram_speeds,'SSD': ssds, 'HDD': hdds, 'Card Brands': card_brands,
+         'Card Models': card_models, 'Prices (Rupee)': pricee, 'Prices (VND)': prices}
+    df = pd.DataFrame.from_dict(a, orient='index')
+    df = df.transpose()
+    dataset = pd.DataFrame(data=df)
+    dataset.to_csv('laptops-flipkart-{}.csv'.format(num_cv))
 
-# def selenium(fromm, to, num_csv, name_thread):
-def selenium():
+def clear_info():
+    for string in generals:
+        remove_n = string.replace("\n", ": ")
+        if "Processor Brand:" in remove_n:
+            chip_brand.append(remove_n)
+
+        if "Clock Speed:" in remove_n:
+            chip_speed.append(remove_n)
+
+        if "Processor Name:" in remove_n:
+            chip_type.append(remove_n)
+
+        if "RAM:" in remove_n:
+            ram.append(remove_n)
+
+        if "RAM Type:" in remove_n:
+            ram_type.append(remove_n)
+
+        if "SSD:" in remove_n:
+            ssd.append(remove_n)
+
+        if "HDD Capacity:" in remove_n:
+            hdd.append(remove_n)
+
+        if "Graphic Processor:" in remove_n:
+            card_model.append(remove_n)
+
+        if "Screen Size:" in remove_n:
+            inch.append(remove_n)
+
+        if "RAM Frequency:" in remove_n:
+            ram_speed.append(remove_n)
+
+    for z in chip_brand:
+        new_string_chip_brand = z.replace("Processor Brand: ", "")
+        chip_brands.append(new_string_chip_brand)
+        print("chip_brands: ", chip_brands)
+
+    for z in chip_speed:
+        new_string_chip_speed = z.replace("Clock Speed: ", "")
+        chip_speeds.append(new_string_chip_speed)
+        print("chip_speeds: ", chip_speeds)
+
+    for z in chip_type:
+        new_string_chip_type = z.replace("Processor Name: ", "")
+        chip_types.append(new_string_chip_type)
+        print("chip_types: ", chip_types)
+
+    for z in ram:
+        new_string_rams = z.replace("RAM: ", "")
+        rams.append(new_string_rams)
+        print("rams: ", rams)
+
+    for z in ram_type:
+        new_string_rams_type = z.replace("RAM Type: ", "")
+        ram_types.append(new_string_rams_type)
+        print("ram_types: ", ram_types)
+
+    for z in ram_speed:
+        new_string_rams_speed = z.replace("RAM Frequency: ", "")
+        ram_speeds.append(new_string_rams_speed)
+        print("ram_speeds: ", ram_speeds)
+
+    for z in ssd:
+        new_string_ssd = z.replace("SSD: ", "")
+        ssds.append(new_string_ssd)
+        print("ssds: ", ssds)
+
+    for z in hdd:
+        new_string_hdd = z.replace("HDD Capacity: ", "")
+        hdds.append(new_string_hdd)
+        print("hdds: ", hdds)
+
+    for z in card_model:
+        new_string_card_model = z.replace("Graphic Processor: ", "")
+        if "Intel" in z:
+            card_brands.append("Intel")
+        elif "AMD" in z:
+            card_brands.append("AMD")
+        elif "NVIDIA" in z:
+            card_brands.append("NVIDIA")
+        elif "Qualcomm" in z:
+            card_brands.append("Qualcomm")
+        elif "MediaTek" in z:
+            card_brands.append("MediaTek")
+        else:
+            card_brands.append("NO")
+        card_models.append(new_string_card_model)
+        print("card_models: ", card_models)
+        print("card_brands: ", card_brands)
+
+    for z in inch:
+        new_string_inch = z.replace("Screen Size: ", "")
+        inches.append(new_string_inch)
+        print("inches: ", inches)
+
+def change_money():
+    for string in pricee:
+        new_string_price = string.replace("₹", "")
+        new_string_price = new_string_price.replace(",", ".")
+        new_string_price = float(new_string_price) * 313.73
+        new_string_price = round(new_string_price, 3)
+        new_string_price = "{:0,.3f}".format(new_string_price)
+        new_string_price = new_string_price.replace(",", ".")
+        prices.append(new_string_price)
+    print("prices (vnd): ", prices)
+
+def selenium(fromm, to, num_csv, name_thread):
+# def selenium():
     try:
         # print(name_thread)
         options = webdriver.ChromeOptions()
@@ -35,211 +161,77 @@ def selenium():
         options.add_argument("--disable-notifications")
         options.add_argument("--disable-popup-blocking")
         browser = webdriver.Chrome(executable_path="./chromedriver.exe", chrome_options=options)
-        #for i in range(1, 12):
-        browser.get("https://mega.com.vn/tim?scat_id=&q=laptop&page=1")
-        sleep(1)
-        click_img = browser.find_elements_by_css_selector(".cdt-product__img")
-        print(len(click_img))
-        # for x in range(from, to):
-        for x in range(0, len(click_img)):
-            click_img = browser.find_elements_by_css_selector(".cdt-product__img")[x]
-            click_img.click()
+        for i in range(fromm, to):
+        # for i in range(1, 2):
+            browser.get("https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page={}".format(i))
             sleep(1)
-
-            try:
-                more_detail_btn = browser.find_element_by_xpath("/html/body/div[3]/div[4]/div[5]/div/div[2]/a")
-                more_detail_btn.click()
+            click_img = browser.find_elements_by_class_name("_4rR01T")
+            # print(len(click_img))
+            # for x in range(from, to):
+            for x in range(0, len(click_img)):
+                main_window = browser.current_window_handle
+                click_img = browser.find_elements_by_class_name("_4rR01T")[x]
+                click_img.click()
+                browser.switch_to_window(browser.window_handles[1])
                 sleep(1)
 
-                chip_brand_name = browser.find_element_by_css_selector("#full-spec > table > tbody > tr:nth-child(1)")
-                if "Sản phẩm" in chip_brand_name:
-                    chip_brand_name = browser.find_element_by_css_selector("#full-spec > table > tbody > tr:nth-child(2)")
-                chip_brands.append(chip_brand_name.text)
-                # sleep(1)
-                print("chip_brands: ", chip_brands)
+                try:
+                    description = browser.find_element_by_class_name("B_NuCI")
+                    # print("description: ", description.text)
+                    descs.append(description.text)
 
-                # chip_speed = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[1]/table[1]/tbody/tr[6]/td[2]")
-                # chip_speeds.append(chip_speed.text)
-                # # sleep(1)
-                # # print("chip_speeds: ", chip_speeds)
-                #
-                # chip_type_1 = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[1]/table[1]/tbody/tr[2]/td[2]")
-                # chip_type_2 = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[1]/table[1]/tbody/tr[3]/td[2]")
-                # chip_types.append(chip_type_1.text + " " + chip_type_2.text)
-                # # sleep(1)
-                # # print("chip_types: ", chip_types)
-                #
-                # ram = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[2]/table[1]/tbody/tr[1]/td[2]")
-                # rams.append(ram.text)
-                # # sleep(1)
-                # # print("rams: ", rams)
-                #
-                # ram_type = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[2]/table[1]/tbody/tr[2]/td[2]")
-                # ram_types.append(ram_type.text)
-                # # sleep(1)
-                # # print("ram_types: ", ram_types)
-                #
-                # ram_speed = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[2]/table[1]/tbody/tr[3]/td[2]")
-                # ram_speeds.append(ram_speed.text)
-                # # sleep(1)
-                # # print("ram_speeds: ", ram_speeds)
-                #
-                # try:
-                #     ssd = browser.find_element_by_xpath(
-                #         "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[5]/div[2]/div[1]/table/tbody/tr[2]/td[2]")
-                #     if ssd.is_displayed():
-                #         ssds.append(ssd.text)
-                #         # sleep(1)
-                #         # print("ssds: ", ssds)
-                # except:
-                #     ssds.append("--")
-                #     # sleep(1)
-                #     # print("ssds: ", ssds)
-                # try:
-                #     hdd = browser.find_element_by_xpath(
-                #         "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[5]/div[2]/div[2]/table/tbody/tr[2]/td[2]")
-                #     if hdd.is_displayed():
-                #         hdds.append(hdd.text)
-                #         # sleep(1)
-                #         # print("hdds: ", hdds)
-                # except:
-                #     hdds.append("--")
-                #     # sleep(1)
-                #     # print("hdds: ", hdds)
-                #
-                # try:
-                #     card_brand_name = browser.find_element_by_xpath(
-                #         "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[4]/div[2]/div[1]/table/tbody/tr[1]/td[2]")
-                #     if card_brand_name.is_displayed():
-                #         card_brands.append(card_brand_name.text)
-                #         # sleep(1)
-                #         # print("card_brands: ", card_brands)
-                # except:
-                #     card_brands.append("--")
-                #     # sleep(1)
-                #     # print("card_brands: ", card_brands)
-                # try:
-                #     card_onboard_name = browser.find_element_by_xpath(
-                #         "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[4]/div[2]/div[2]/table/tbody/tr[1]/td[2]")
-                #     if card_onboard_name.is_displayed():
-                #         card_onboards.append(card_onboard_name.text)
-                #         # sleep(1)
-                #         # print("card_onboards: ", card_onboards)
-                # except:
-                #     card_onboards.append("--")
-                #     # sleep(1)
-                #     # print("card_onboards: ", card_onboards)
-                #
-                # try:
-                #     card_brand_model = browser.find_element_by_xpath(
-                #         "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[4]/div[2]/div[1]/table/tbody/tr[2]/td[2]")
-                #     if card_brand_model.is_displayed():
-                #         card_models.append(card_brand_model.text)
-                #         # sleep(1)
-                #         # print("card_models: ", card_models)
-                # except:
-                #     card_models.append("--")
-                #     # sleep(1)
-                #     # print("card_brands: ", card_brands)
-                # try:
-                #     card_onboard_model = browser.find_element_by_xpath(
-                #         "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[4]/div[2]/div[2]/table/tbody/tr[2]/td[2]")
-                #     if card_onboard_model.is_displayed():
-                #         card_onboard_models.append(card_onboard_model.text)
-                #         # sleep(1)
-                #         # print("card_onboard_models: ", card_onboard_models)
-                # except:
-                #     card_onboard_models.append("--")
-                #     # sleep(1)
-                #     # print("card_onboard_models: ", card_onboard_models)
-                #
-                # inch = browser.find_element_by_xpath(
-                #     "/html/body/div[2]/main/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[3]/div[3]/table[1]/tbody/tr[1]/td[2]")
-                # inches.append(inch.text)
-                # # print("inches: ", inches)
-                #
-                # # details = browser.find_elements_by_css_selector(".product-summary li")
-                # # detail = [el.text for el in details]
-                # # # print("detail: ", detail)
-                # # prices = browser.find_elements_by_css_selector("#product-info-price p:nth-child(2) b")
-                # # if len(prices) > 0:
-                # #     price = [el.text for el in prices]
-                # # else:
-                # #     prices = browser.find_elements_by_css_selector("#product-info-price b")
-                # #     price = [el.text for el in prices]
-                # # # price = [el.text for el in prices]
-                # # # print("price: ", price)
-                # # for i in range(0, len(detail)):
-                # #     if ("CPU" in detail[i]):
-                # #         processors.append(detail[i])
-                # #         for string in processors:
-                # #             new_string_pro = string.replace("- CPU: ", "")
-                # #             processor.append(new_string_pro)
-            except:
-                browser.back()
+                    try:
+                        price = browser.find_element_by_class_name("_17Rl6L")
+                        print("price: ", price.text)
+                        pricee.append(price.text)
+                    except:
+                        price = browser.find_element_by_class_name("_30jeq3")
+                        print("price: ", price.text)
+                        pricee.append(price.text)
 
-        browser.close()
-        # browser.back()
+                    more_detail_btn = browser.find_element_by_class_name("_1FH0tX")
+                    more_detail_btn.click()
 
-        sleep(1)
-        a = {'Brands': descs, 'Chip Brands': chip_brands, 'Chip Speeds': chip_speeds, 'Chip Type': chip_types,
-             'Ram': rams, 'Ram Type': ram_types, 'SSD': ssds, 'HDD': hdds, 'Card Brands': card_brands,
-             'Card Models': card_models, 'Card Onboards': card_onboards, 'Card Onboard Models': card_onboard_models,
-             'Prices': prices}
-        df = pd.DataFrame.from_dict(a, orient='index')
-        df = df.transpose()
-        dataset = pd.DataFrame(data=df)
-        dataset.to_csv('laptops-fpt.csv')
-        browser.close()
+                    general = browser.find_elements_by_css_selector("._14cfVK tr")
+                    global generals
+                    generals = [el.text for el in general]
+                    # print("generals: ", generals)
+
+                    change_money()
+                    clear_info()
+                    sleep(1)
+                    browser.close()
+                    browser.switch_to_window(main_window)
+                    sleep(1)
+                    # browser.back()
+                except:
+                    browser.back()
+
+        save()
+        # browser.close()
     except:
-        # a = {'Brands': descs, 'Chip Brands': chip_brands, 'Chip Speeds': chip_speeds, 'Chip Type': chip_types,
-        #      'Ram': rams, 'Ram Type': ram_types, 'SSD': ssds, 'HDD': hdds, 'Card Brands': card_brands,
-        #      'Card Models': card_models, 'Card Onboards': card_onboards, 'Card Onboard Models': card_onboard_models,
-        #      'Prices': prices}
-        #
-        # df = pd.DataFrame.from_dict(a, orient='index')
-        # df = df.transpose()
-        # dataset = pd.DataFrame(data=df)
-        # dataset.to_csv('laptops-fpt.csv')
-        pass
+        save()
+
 
 
 if __name__ == '__main__':
-    selenium()
-    # threads = []
-    # name_thread_1 = "thread 1"
-    # name_thread_2 = "thread 2"
-    # name_thread_3 = "thread 3"
-    # name_thread_4 = "thread 4"
-    # name_thread_5 = "thread 5"
-    # name_thread_6 = "thread 6"
-    # name_thread_7 = "thread 7"
-    # t1 = threading.Thread(target=selenium, args=(0, 153, 1, name_thread_1))
-    # t1.start()
-    # t2 = threading.Thread(target=selenium, args=(153, 306, 2, name_thread_2))
-    # t2.start()
-    # t3 = threading.Thread(target=selenium, args=(396, 459, 3, name_thread_3))
-    # t3.start()
-    # t4 = threading.Thread(target=selenium, args=(459, 612, 4, name_thread_4))
-    # t4.start()
-    # t5 = threading.Thread(target=selenium, args=(612, 765, 5, name_thread_5))
-    # t5.start()
-    # t6 = threading.Thread(target=selenium, args=(765, 918, 6, name_thread_6))
-    # t6.start()
-    # t7 = threading.Thread(target=selenium, args=(918, 1071, 7, name_thread_7))
-    # t7.start()
-    # threads.append(t1)
-    # threads.append(t2)
-    # threads.append(t3)
-    # threads.append(t4)
-    # threads.append(t5)
-    # threads.append(t6)
-    # threads.append(t7)
-    # for th in threads:
-    #     th.join()
+    # selenium()
+    threads = []
+    name_thread_1 = "thread 1"
+    name_thread_2 = "thread 2"
+    name_thread_3 = "thread 3"
+    name_thread_4 = "thread 4"
+    t1 = threading.Thread(target=selenium, args=(1, 9, 1, name_thread_1))
+    t1.start()
+    t2 = threading.Thread(target=selenium, args=(9, 16, 2, name_thread_2))
+    t2.start()
+    t3 = threading.Thread(target=selenium, args=(16, 23, 3, name_thread_3))
+    t3.start()
+    t4 = threading.Thread(target=selenium, args=(23, 30, 4, name_thread_4))
+    t4.start()
+    threads.append(t1)
+    threads.append(t2)
+    threads.append(t3)
+    threads.append(t4)
+    for th in threads:
+        th.join()
